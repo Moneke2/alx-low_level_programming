196b5c6ebc1342e1b5a2baa560ffb50149bcb758
Moneke2 <benjamin.moneke@gmail.com>
2023-08-11 20:55:34 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+#include<stdio.h>
 /**
  *  main - Entry point
  *
@@ -7,6 +7,6 @@
 int main(void)
 {														
        	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
 		          
