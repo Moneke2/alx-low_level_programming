e1be78f0199b059080447c1c3bfd3de550947928
Moneke2 <benjamin.moneke@gmail.com>
2023-08-11 21:39:12 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(vi=oid)
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
