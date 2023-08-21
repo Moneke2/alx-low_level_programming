098a45acfb129fd7440c5da5287638676283cfad
Moneke2 <benjamin.moneke@gmail.com>
2023-08-21 11:43:24 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@
 int _atoi(char *s)
 
 {
-int c =0;
+int c = 0;
 unsigned int ni = 0;
 int min = 1;
 int isi = 0;
@@ -18,9 +18,9 @@ while (s[c])
 {
 if (s[c] == 45)
 {
-min *= - 1;
+min *= -1;
 }
-while(s[c] >= 48 && s[c] <= 57)
+while (s[c] >= 48 && s[c] <= 57)
 {
 isi = 1;
 ni = (ni * 10) + (s[c] - '0');
