769e39b3bc6aa1965a5e72114d276eb6824c8ed4
Moneke2 <benjamin.moneke@gmail.com>
2023-08-24 14:28:27 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,7 +16,7 @@ char *rot13(char *s)
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < s2; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
